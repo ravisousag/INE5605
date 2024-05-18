5cf07add900478699afe339c41b6a103ed73f4f4
Ravi <ravisousagarcindo@gmail.com>
2024-05-18 03:16:51 -0300

    Add classe main

---
+++ code/Main.py
@@ -0,0 +1,15 @@
+import getpass
+
+class Main:
+
+
+    login = str(input("""
+    ============ Bem vindo ao JetEase ============
+    Insira seu login: """))
+    password = getpass.getpass("""
+    Insira sua senha: """
+    )
+    
+    def menu():
+        print(""" 
+    """)
