3039e7f3d3ed487e8799ab6b16e184cced1be9ec
edwirgescv <139360246+edwirgescv@users.noreply.github.com>
2024-05-20 14:01:35 -0300

    Add files via upload

---
+++ code/Entitys/transactionitem.py
@@ -0,0 +1,26 @@
+from inventoryitem import InventoryItem, abstractmethod
+from part import Part, abstractmethod
+
+class TransactionItem(part, Inventory):
+
+    @abstractmethod
+    def__init__(self):
+        pass
+
+
+    class transfer(transactionitem):
+        self.__id_part = id_part
+        return id_part
+    
+    def qt_stock(inventoryitem)
+        Part = 0
+        for Part in Part:
+            qt_stock += Parts
+            return qt_stock
+        
+    
+        
+        
+    
+
+    
