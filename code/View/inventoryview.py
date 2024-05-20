20f2dc69c00b6225d8dc480d23af880becd30dea
edwirgescv <139360246+edwirgescv@users.noreply.github.com>
2024-05-20 14:02:15 -0300

    Add files via upload

--- code/View/inventoryview.py
+++ code/View/inventoryview.py
@@ -1,32 +1,32 @@
+
 class InventoryView:
 
     def __init__(self, InventoryController)
         self.__inventorycontroller = InventoryControllerController
 
-    def le_num_inteiro(self_ mensagem: str = "", inteiros_validos
+    def le_num_inteiro(self_ mensagem: str = "", inteiros_validos: [] = None):
+while True:
+    Valor_lido = input (mensagem)
 
-    : [] = None):
-    while True:
-        Valor_lido = input(mensagem)
+try:
+    inteiro: = int(valor_lido)
+    if inteiros_validos and inteiro not in inteiros_validos:
+        raise ValueError
+    return inteiro 
+except ValueError:
+    print("Valor incorreto: Digite um valor numerico inteiro valido")
+    if inteiros_validos:
+        print("Valor validos:", inteiros_validos)
 
-    try:
-        inteiro: = int(valor_lido)
-        if inteiros_validos and inteiro not in inteiros_validos:
-            raise ValueError
-        return inteiro
-    except ValueError:
-        print("Valor incorreto: Digite um valor numerico inteiro valido")
-        if inteiros_validos:
-            print("Valor validos:", inteiros_validos)
+def mostra_tela_opcoes(self):
+    print("----------Estoque----------")
+    print("----Escolha uma ooção----")
+    print("1 - Listar quantidade de Peças")
+    print("0 - Retornar")
+    opcao = self.le_num_inteiro("Escolha a opcao:", [1, 0])
+    return opcao
 
-    def mostra_tela_opcoes(self):
-        print("----------Estoque----------")
-        print("----Escolha uma ooção----")
-        print("1 - Listar quantidade de Peças")
-        print("0 - Retornar")
-        opcao = self.le_num_inteiro("Escolha a opcao:", [1, 0])
-        return opcao
-
-
+    
 
 
+    
--- code/View/invoiceview.py
+++ code/View/invoiceview.py
@@ -1,35 +1,35 @@
+
 class InvoiceView:
 
     def __init__(self, InvoiceController)
         self.__invoicecontroller = InvoiceControllerController
 
-    def le_num_inteiro(self_ mensagem: str = "", inteiros_validos
+    def le_num_inteiro(self_ mensagem: str = "", inteiros_validos: [] = None):
+while True:
+    Valor_lido = input (mensagem)
 
-    : [] = None):
-    while True:
-        Valor_lido = input(mensagem)
+try:
+    inteiro: = int(valor_lido)
+    if inteiros_validos and inteiro not in inteiros_validos:
+        raise ValueError
+    return inteiro 
+except ValueError:
+    print("Valor incorreto: Digite um valor numerico inteiro valido")
+    if inteiros_validos:
+        print("Valor validos:", inteiros_validos)
 
-    try:
-        inteiro: = int(valor_lido)
-        if inteiros_validos and inteiro not in inteiros_validos:
-            raise ValueError
-        return inteiro
-    except ValueError:
-        print("Valor incorreto: Digite um valor numerico inteiro valido")
-        if inteiros_validos:
-            print("Valor validos:", inteiros_validos)
+def mostra_tela_opcoes(self):
+    print("----------Part----------")
+    print("----Escolha uma ooção----")
+    print("1 - Incluir Fatura")
+    print("2 - Alterar Fatura")
+    print("3 - Relatório de faturamento")
+    print("4 - Excluir Fatura")
+    print("0 - Retornar")
+    opcao = self.le_num_inteiro("Escolha a opcao:", [1, 2, , 3, 4, 5, 0])
+    return opcao
 
-    def mostra_tela_opcoes(self):
-        print("----------Part----------")
-        print("----Escolha uma ooção----")
-        print("1 - Incluir Fatura")
-        print("2 - Alterar Fatura")
-        print("3 - Relatório de faturamento")
-        print("4 - Excluir Fatura")
-        print("0 - Retornar")
-        opcao = self.le_num_inteiro("Escolha a opcao:", [1, 2,, 3, 4, 0])
-        return opcao
-
-
+    
 
 
+    
